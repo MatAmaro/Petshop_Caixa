96b7e4668095d329cc4a6199a21ac5b5b1506765
matam <matam@LAPTOP-HF2CEEJK>
2022-06-10 20:38:21 -0300

    Adicionar arquivos de projeto.

---
+++ Petshop_Caixa/Caixa.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petshop_Caixa
+{
+     public class Caixa
+     { 
+        public DateTime DataCaixa { get; private set; }
+        public decimal Saldo { get; private set; }
+
+        public void AbrirCaixa(decimal saldoInicial = 0)
+        {
+            DataCaixa = DateTime.Now;
+            Saldo = saldoInicial;
+        }
+
+        public void Deposito(decimal valor)
+        {
+            Saldo += valor;
+        }
+        public void Saque(decimal valor)
+        {
+            if (Saldo >= valor)
+            {
+                Saldo -= valor;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente");
+                Console.WriteLine($"o saldo atual é de R${Saldo}");
+            }
+
+        }
+        public decimal CalcularTroco(decimal valorVenda, decimal dinheiroEntregue)
+        {
+            var troco = 0M;
+            if (valorVenda <= dinheiroEntregue)
+            {
+                troco = dinheiroEntregue - valorVenda;
+            }
+            else
+            {
+                troco = -1M;
+            }
+
+            return troco;
+        }
+
+        public void DevolverTroco(decimal troco, decimal valorVenda)
+        {
+            Deposito(valorVenda);
+            Console.WriteLine($"Troco: R${troco}");
+            Console.WriteLine($"Saldo atual do caixa após a venda R${Saldo}");
+
+        }
+    }
+}
---
+++ Petshop_Caixa/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Petshop_Caixa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Caixa caixaDoDia = new Caixa();
+            caixaDoDia.AbrirCaixa(500);
+
+            Console.WriteLine($"Caixa Aberto em { caixaDoDia.DataCaixa }");
+            Console.WriteLine($"O saldo atual é de R${caixaDoDia.Saldo}");
+            Console.WriteLine("Nova Venda");
+
+            Console.WriteLine("Qual o valor da compra?");
+            var compra = Console.ReadLine();
+            decimal.TryParse(compra, out decimal valorCompra);
+            Console.WriteLine("Qual o valor entregue pelo clinete?");
+            var pago = Console.ReadLine();
+            decimal.TryParse(pago, out decimal valorPago);
+            var troco = caixaDoDia.CalcularTroco(valorCompra, valorPago);
+            if (troco != -1M)
+            {
+                caixaDoDia.DevolverTroco(troco, valorCompra);
+            }
+            else
+            {
+                Console.WriteLine("Valor entregue insuficiente");
+            }
+        }
+    }
+}
